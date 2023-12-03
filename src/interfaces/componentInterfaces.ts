26e692afc07f88d7edb0066ee339ee0ceadc5078
Matthew Banuag <matthewbanuag.dev@gmail.com>
2023-12-03 04:05:32 -0600

    Update componentInterfaces.ts with new SectionProps interface

--- src/interfaces/componentInterfaces.ts
+++ src/interfaces/componentInterfaces.ts
@@ -1,6 +1,9 @@
 export interface PageContainerProps {
     children: ChildNode;
 }
+export interface SectionProps {
+    isTwoColumns?: boolean;
+}
 
 export interface TextProps {
     bold?: boolean;
