3b021037850a3e1462f3302a6fcbcbe31ccc685b
Matthew Banuag <matthewbanuag.dev@gmail.com>
2023-12-03 05:20:53 -0600

    Update Images.style.ts with new LandingImage nad Image styled components

---
+++ src/components/media/Images.style.ts
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const LandingImage = styled.img`
+    border-radius: 10%;
+    width: 250px;
+    height: auto;
+`;
+
+export const Image = styled.img`
+
+`;
