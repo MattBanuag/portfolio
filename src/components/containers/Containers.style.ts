fa2cf1ed3cb951e4006609343e7f78d24df41b94
Matthew Banuag <matthewbanuag.dev@gmail.com>
2023-12-03 02:18:22 -0600

    Update Container.style.ts with new styled components

--- src/components/containers/Containers.style.ts
+++ src/components/containers/Containers.style.ts
@@ -5,3 +5,13 @@ export const PageContainer = styled.main`
     height: 100vh;
     margin: 5% 20%;
 `;
+
+export const Section = styled.section`
+
+`;
+
+export const Article = styled.article`
+    display: flex;
+    flex-direction: column;
+    gap: 20px;
+`;
