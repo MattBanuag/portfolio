4fa08e1ad0c65624da2f2f9b717354f04936091c
Matthew Banuag <matthewbanuag.dev@gmail.com>
2023-12-03 04:06:19 -0600

    Update Container.style.ts with Section styled component styling

--- src/components/containers/Containers.style.ts
+++ src/components/containers/Containers.style.ts
@@ -1,12 +1,18 @@
 import styled from "styled-components";
-import { PageContainerProps } from "../../interfaces/componentInterfaces";
+import { PageContainerProps, SectionProps } from "../../interfaces/componentInterfaces";
 
 export const PageContainer = styled.main`
     height: 100vh;
     margin: 5% 20%;
 `;
 
-export const Section = styled.section`
+export const Section = styled.section<SectionProps>`
+    display: flex;
+    flex-direction: ${({ isTwoColumns }) => !isTwoColumns && "column"};
+    gap: 20px;
+`;
+
+export const Div = styled.div`
 
 `;
 
