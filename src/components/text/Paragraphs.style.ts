6dc7caf6e9a3a0f4efe4cf041cac312139034af0
Matthew Banuag <matthewbanuag.dev@gmail.com>
2023-12-03 04:06:46 -0600

    Update Paragraph.style.ts with line-height property

--- src/components/text/Paragraphs.style.ts
+++ src/components/text/Paragraphs.style.ts
@@ -4,5 +4,6 @@ import { TextProps } from "../../interfaces/componentInterfaces";
 export const Text = styled.p<TextProps>`
     font-size: ${({ size }) => size === null ? "1.1rem" : size };
     font-weight: ${({ bold }) => bold ? "600" : "400" };
+    line-height: 1.8rem;
     color: ${({ color }) => color !== null && color };
 `;
