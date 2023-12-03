d5e1e7c1fa677867173aea2947d17bb5419d245a
Matthew Banuag <matthewbanuag.dev@gmail.com>
2023-12-03 00:54:17 -0600

    Update Container.style.ts with PageContainer styled component

---
+++ src/components/containers/Containers.style.ts
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+import { PageContainerProps } from "../../interfaces/componentInterfaces";
+
+export const PageContainer = styled.main`
+    height: 100vh;
+    margin: 5% 20%;
+`;
