0347c7ddeaa0b6a4f1004b86c32c436b20508c62
Matthew Banuag <matthewbanuag.dev@gmail.com>
2023-12-03 00:55:49 -0600

    Update FAB.style.ts FABContainer styled component with styles

--- src/components/fab/FAB.style.ts
+++ src/components/fab/FAB.style.ts
@@ -1,5 +1,22 @@
 import styled from "styled-components";
 
-const FABContainer = styled.div`
-    
+export const FABContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    position: fixed;
+    top: 10%;
+    right: 20%;
+    padding: 20px;
+    border: 1px solid #F2EDE9;
+    border-radius: 50%;
+    cursor: pointer;
+    box-shadow: rgba(99, 99, 99, 0.2) 0px 2px 8px 0px;
+    background-color: #F2EDE9;
+    transition: ease-in-out 0.2s;
+
+    &:hover {
+        border: 0.1px solid #41474C;
+    }
 `;
