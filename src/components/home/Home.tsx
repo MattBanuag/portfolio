ffcb4baf9e8bfad84609ab095eb1ad4b4bef865b
Matthew Banuag <matthewbanuag.dev@gmail.com>
2023-12-02 22:51:16 -0600

    Update Home.tsx with PageContainer component

---
+++ src/components/home/Home.tsx
@@ -0,0 +1,11 @@
+import { PageContainer } from "../containers/Containers.style"
+
+const Home = () => {
+  return (
+    <PageContainer>
+        
+    </PageContainer>
+  )
+}
+
+export default Home
